0ee3894b94396c00ca905016d27ddeeaea7084ba
Igor Machado <igor.machado@gmail.com>
2019-04-19 11:47:52 -0300

    added hex count

--- src/core/system/vhelper.h
+++ src/core/system/vhelper.h
@@ -58,11 +58,12 @@ public:
 
 ostream& operator<<(ostream &os, const vbyte &obj)
 {
-	os << "0x";
+   stringstream ss; // do not dirty 'os' with specific formatting
+	ss << "[" << obj.size() << "]0x";
 	if (obj.size() > 0)
 		for (unsigned int i = 0; i < obj.size(); i++)
-			os << std::setfill('0') << std::setw(2) << std::hex << (int)obj[i];
-	return os;
+			ss << std::setfill('0') << std::setw(2) << std::hex << (int)obj[i];
+	return os << ss.str();
 }
 
 
