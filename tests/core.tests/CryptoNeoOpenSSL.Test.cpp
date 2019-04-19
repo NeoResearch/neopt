c52678bca26f28c782388e4d1461bbf48365b970
Igor Machado <igor.machado@gmail.com>
2019-04-19 14:26:42 -0300

    tests for signature verification

--- tests/core.tests/CryptoNeoOpenSSL.Test.cpp
+++ tests/core.tests/CryptoNeoOpenSSL.Test.cpp
@@ -34,3 +34,21 @@ TEST(CryptoTest, Test_Hash256_Zero)
    vbyte v(1, 0); // 0x00
 	EXPECT_EQ(crypto.Hash256(v), crypto.Sha256(crypto.Sha256(v)));
 }
+
+// verification tests
+
+TEST(CryptoTest, Test_SignData_EmptyMessage)
+{
+	CryptoNeoOpenSSL crypto;
+   vbyte msg(0); // '': empty message
+
+   // creating private/public key pair (random each test)
+   vbyte mypubkey;
+   vbyte myprivkey = crypto.GeneratePrivateKey(mypubkey);
+
+   // sign empty message
+   vbyte sig = crypto.SignData(crypto.Sha256(msg), myprivkey, mypubkey);
+
+   // test if signature matches public key for message
+	EXPECT_EQ(crypto.VerifySignature(msg, sig, mypubkey), 1);
+}
