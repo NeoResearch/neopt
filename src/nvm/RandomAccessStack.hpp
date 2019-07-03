b258203f6f05bcdf590eecb8c2c6a061607e13f9
Igor Machado <igor.machado@gmail.com>
2019-07-03 17:08:38 -0300

    double check RandomAccessStack

--- src/nvm/RandomAccessStack.hpp
+++ src/nvm/RandomAccessStack.hpp
@@ -16,9 +16,117 @@ using namespace std; // TODO: remove
 
 namespace neopt {
 namespace nvm {
-class RandomAccessStack
-{
 
+// TODO: this class is currently intended to work with pointers, but it can be changed to ref/moved based
+
+template<class T>
+class RandomAccessStack // TODO: IReadOnlyCollection<T> (does it help in anything?)
+{
+private:
+   vector<T*> list;
+
+public:
+   int Count()
+   {
+      return list.size();
+   }
+
+   void Clear()
+   {
+      list.clear();
+   }
+
+   void CopyTo(RandomAccessStack<T>& stack, int count = -1)
+   {
+      if (count == 0)
+         return;
+      if (count == -1)
+         stack.list.AddRange(list);
+      else
+         stack.list.AddRange(list.Skip(list.Count - count));
+   }
+
+   //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+   //public IEnumerator<T> GetEnumerator()
+   //{
+   //    return list.GetEnumerator();
+   //}
+
+   //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+   //IEnumerator IEnumerable.GetEnumerator()
+   //{
+   //    return list.GetEnumerator();
+   //}
+
+   void Insert(int index, T* item)
+   {
+      if (index > list.size()) {
+         // throw new InvalidOperationException();
+         NEOPT_EXCEPTION("InvalidOperationException");
+      }
+      list.insert(list.begin() + Count() - index, item);
+   }
+
+   T* Peek(int index = 0)
+   {
+      if (index >= list.size()) {
+         // throw new InvalidOperationException();
+         NEOPT_EXCEPTION("InvalidOperationException");
+      }
+      if (index < 0) {
+         index += list.size();
+         if (index < 0) {
+            //throw new InvalidOperationException();
+            NEOPT_EXCEPTION("InvalidOperationException");
+         }
+      }
+      return list[(Count() - index - 1)];
+   }
+
+   T* Pop()
+   {
+      return Remove(0);
+   }
+
+   void Push(T* item)
+   {
+      list.push_back(item);
+   }
+
+   T* Remove(int index)
+   {
+      if (index >= Count()) {
+         //throw new InvalidOperationException();
+         NEOPT_EXCEPTION("InvalidOperationException");
+      }
+      if (index < 0) {
+         index += Count();
+         if (index < 0) {
+            //throw new InvalidOperationException();
+            NEOPT_EXCEPTION("InvalidOperationException");
+         }
+      }
+      index = Count() - index - 1;
+      T* item = list[index];
+      list.erase(list.begin() + index);
+      return item;
+   }
+
+   void Set(int index, T* item)
+   {
+      if (index >= Count()) {
+         //throw new InvalidOperationException();
+         NEOPT_EXCEPTION("InvalidOperationException");
+      }
+      if (index < 0) {
+         index += Count();
+         if (index < 0) {
+            //throw new InvalidOperationException();
+            NEOPT_EXCEPTION("InvalidOperationException");
+         }
+      }
+      list[(Count() - index - 1)] = item;
+   }
 };
 
 } // namespace nvm
